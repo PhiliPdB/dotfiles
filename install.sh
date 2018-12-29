98492491805d47d0584aaecb1e169059ef867ede
PhiliPdB <phlpdbrn@gmail.com>
2018-12-29 14:19:02 +0100

    Small updates

--- install.sh
+++ install.sh
@@ -1,4 +1,24 @@
+#! /bin/bash
+
+# Get the current directory
+DIR=`pwd`
+
+# Profile
+rm -f ~/.profile
+ln -s $DIR/.profile ~/.profile
+
+# Bash
+rm -f ~/.bashrc ~/.bash_aliases ~/.bash_logout
+ln -s $DIR/bash/.bashrc ~/.bashrc
+ln -s $DIR/bash/.bash_aliases ~/.bash_aliases
+ln -s $DIR/bash/.bash_logout ~/.bash_logout
 
 
-# TODO
+# Emacs
+rm -f ~/.emacs
+rm -rf ~/.emacs.d
+ln -s $DIR/emacs/.emacs ~/.emacs
+ln -s $DIR/emacs/.emacs.d ~/.emacs.d
+
+
 
