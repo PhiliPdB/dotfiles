72dd3554663d2e8c6366848d07d5e66bcda71405
PhiliPdB <phlpdbrn@gmail.com>
2018-12-29 14:17:20 +0100

    Finish setup script

--- setup.sh
+++ setup.sh
@@ -8,11 +8,17 @@ rm -f ~/.profile
 ln -s $DIR/.profile ~/.profile
 
 # Bash
+rm -f ~/.bashrc ~/.bash_aliases ~/.bash_logout
+ln -s $DIR/bash/.bashrc ~/.bashrc
+ln -s $DIR/bash/.bash_aliases ~/.bash_aliases
+ln -s $DIR/bash/.bash_logout ~/.bash_logout
 
 
 # Emacs
-
-
+rm -f ~/.emacs
+rm -rf ~/.emacs.d
+ln -s $DIR/emacs/.emacs ~/.emacs
+ln -s $DIR/emacs/.emacs.d ~/.emacs.d
 
 
 
